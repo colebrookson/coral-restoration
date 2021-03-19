8e36765ca47411d4bed732919d9253fafaf45ec6
ArielGreiner <ariel.greiner@mail.mcgill.ca>
2021-03-18 18:54:15 -0600

    Fixing rounding issues with dispersal bifurcation graphs

--- code/generating_bifurcation_diagrams/CleanMatlabOutput_MakeBifurcationDiagrams.R
+++ code/generating_bifurcation_diagrams/CleanMatlabOutput_MakeBifurcationDiagrams.R
@@ -101,14 +101,14 @@ for(i in 1:length(g_val)){ #loop through the grazing values
     #all   
     #pdf(here("graphs","bifurcationdiagrams","dispersal_bifurcation_diagrams","pdf","Normal",paste0(g_bin,"_grazing"), paste0("DispersalBifurcationGraph_MCcomp",a_val[k],"grazing",g_val[i],".pdf")))
     png(here("graphs","bifurcationdiagrams","dispersal_bifurcation_diagrams","png","Normal",paste0(g_bin,"_grazing"), paste0("DispersalBifurcationGraph_MCcomp",a_val[k],"grazing",g_val[i],".png")))
-    plot(x = allparam_data_abr$z[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == g_val[i]], y = allparam_data_abr$C[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == g_val[i]], col = allparam_data_abr$Colour[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == g_val[i]], xlim = c(0,1), ylim = c(0,1), pch = 16, xlab = "Dispersal", ylab = "C cover", main = paste("MCcomp = ", a_val[k], "and grazing =", g_val[i]))
+    plot(x = allparam_data_abr$z[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == round(g_val[i],2)], y = allparam_data_abr$C[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == round(g_val[i],2)], col = allparam_data_abr$Colour[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == round(g_val[i],2)], xlim = c(0,1), ylim = c(0,1), pch = 16, xlab = "Dispersal", ylab = "C cover", main = paste("MCcomp = ", a_val[k], "and grazing =", g_val[i]))
     legend("topleft", c("Stable Node", "Unstable Node", "Saddle", "Bifurcation Point?"), col = c("black","gold","purple","green"), pch = c(20,20))
     dev.off()
     
     #transparent   
     #pdf(here("graphs","bifurcationdiagrams","dispersal_bifurcation_diagrams","pdf","Transparent",paste0(g_bin,"_grazing"), paste0("Transparent_DispersalBifurcationGraph_MCcomp",a_val[k],"grazing",g_val[i],".pdf")))
     png(here("graphs","bifurcationdiagrams","dispersal_bifurcation_diagrams","png","Transparent",paste0(g_bin,"_grazing"), paste0("Transparent_DispersalBifurcationGraph_MCcomp",a_val[k],"grazing",g_val[i],".png")))
-    plot(x = allparam_data_abr$z[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == g_val[i]], y = allparam_data_abr$C[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == g_val[i]], col = alpha(allparam_data_abr$Colour[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == g_val[i]],0.4), xlim = c(0,1), ylim = c(0,1), pch = 16, xlab = "Dispersal", ylab = "C cover", main = paste("MCcomp = ", a_val[k], "and grazing =", g_val[i]))
+    plot(x = allparam_data_abr$z[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == round(g_val[i],2)], y = allparam_data_abr$C[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == round(g_val[i],2)], col = alpha(allparam_data_abr$Colour[allparam_data_abr$a == a_val[k] & allparam_data_abr$g == round(g_val[i],2)],0.4), xlim = c(0,1), ylim = c(0,1), pch = 16, xlab = "Dispersal", ylab = "C cover", main = paste("MCcomp = ", a_val[k], "and grazing =", g_val[i]))
     legend("topleft", c("Stable Node", "Unstable Node", "Saddle", "Bifurcation Point?"), col = alpha(c("black","gold","purple","green"),0.4), pch = c(20,20))
     dev.off()
     
