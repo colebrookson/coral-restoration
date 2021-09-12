549dff799d67fded7e55d2bee1e950b70677253d
colebrookson <cole.brookson@gmail.com>
2021-09-12 17:10:58 -0400

    add requirements text

---
+++ src/compute-canada-create-env.sh
@@ -0,0 +1,14 @@
+###########################
+###########################
+## This script installs python and the packages needed for analysis, and was 
+## written specifically for use on Compute Canada servers
+###########################
+###########################
+## date: 2021-09-12
+## author: Cole B. Brookson, Ariel G. Greiner
+###########################
+###########################
+
+# load python itself
+module load python/3.9.6
+
