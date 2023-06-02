3151e9c36265d30f5ea05cad5c41f0e979341b86
Cole Brookson <cole.brookson@gmail.com>
2023-06-01 22:16:31 -0400

    figs and the functions to make the data

---
+++ R/functions_generate_data_for_matlab.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(here)
+
+# define variables =============================================================
+
+# a = rate macroalgae overgrows turf algae
+a_vec <- seq(0, 0.7, 0.01)
+
+# g = rate of grazing on algae
+g_vec <- seq(0, 0.99, 0.01)
+
+# z = rate coral larvae recruit and overgrow turf algae
+z_vec <- seq(0, 0.99, 0.01)
+
+equil_nums <- c(1:20)
+
+# put them all in a data.frame
+df <- expand.grid(a = a_vec, g  = g_vec, z = z_vec, 
+                  Equilibrium = equil_nums)
+
+# now add in the other variables that stay consistent
+df$r <- 0.55; df$d <- 0.24; df$y <- 0.77
+df$C <- 2; df$M <- 2; df$eig_1 <- 2; df$eig_2 <- 2
+
+# order them as we had them already 
+df <- df %>% dplyr::select(r, d, y, a, g, z, Equilibrium, C, M, eig_1, eig_2)
+
+# save as a csv
+# readr::write_csv(
+#   df,
+#   here("./data/parameter-data/full-params-for-matlab.csv")
+# )
+
