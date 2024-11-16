0e90aa39220d0ca159505870b40d886972be2dfa
colebrookson <cole.brookson@gmail.com>
2024-11-16 14:10:39 -0500

    saved new plots

--- R/15_all_plotting.R
+++ R/15_all_plotting.R
@@ -1,22 +1,6 @@
 #' I just need a single file to run if I make plotting updates because that
 #' will make my life easier lol
 
-# run this code before calling ggplot2 function
-guides_merge <- function(gdefs) {
-    gdefs <- lapply(gdefs, function(g) {
-        g$hash <-
-            paste(g$order, g$hash, sep = "z")
-        g
-    })
-    tapply(
-        gdefs, sapply(gdefs, function(g) g$hash),
-        function(gs) Reduce(guide_merge, gs)
-    )
-}
-environment(guides_merge) <- environment(ggplot)
-assignInNamespace("guides_merge", guides_merge, pos = "package:ggplot2")
-
-
 source(here::here("./R/07_plot_min_coral.R"))
 source(here::here("./R/08_plot_goodID_props.R"))
 source(here::here("./R/10_plot_min_coral_macro.R"))
